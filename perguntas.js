5b5a4f7d11ce732abe147c0dfe7afbfd66f9e097
Midzz0 <146261452+Midzz0@users.noreply.github.com>
2024-11-05 10:20:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Anime',
+    'Quem é o protagonista do anime Black Clover?',
+    'O Asta'
+)
+
+criaCartao(
+    'Livro em Quadrinhos',
+    'Qual é a HQ mais vendida do mundo?',
+    'X-Men Vol. 1'
+)
+
+criaCartao(
+    'Mangá',
+    'Qual é o Mangá mais lido do mundo?',
+    'One piece'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o pais que mais fazem mangás?',
+    'Japão'
+)
